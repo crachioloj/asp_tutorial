7a350d52fd51cb35a4f8bcf4704dd4d1ce6d3c11
J C <crachioloj@gmail.com>
2021-10-27 21:28:01 -0400

    Pt 71

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+  public class CustomersController : ApiController
+  {
+    private ApplicationDataModel _dbContext;
+
+    public CustomersController()
+    {
+      _dbContext = new ApplicationDataModel();
+    }
+
+    public IEnumerable<CustomerDto> GetCustomers()
+    {
+      return _dbContext.Customers.AsEnumerable()
+        .Select(Mapper.Map<Customer, CustomerDto>);
+    }
+
+    [HttpGet]
+    public IHttpActionResult GetCustomer(int id)
+    {
+      var customer = _dbContext.Customers.SingleOrDefault(c => c.Id == id);
+
+      if (customer is null)
+      {
+        return NotFound();
+      }
+
+      return Ok(Mapper.Map<Customer, CustomerDto>(customer));
+    }
+
+    [HttpPost]
+    public IHttpActionResult CreateCustomer(CustomerDto customerDto)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest();
+      }
+
+      var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+      _dbContext.Customers.Add(customer);
+      _dbContext.SaveChanges();
+
+      customerDto.Id = customer.Id;
+
+      return Created(new Uri($"{Request.RequestUri}/{customer.Id}"), customerDto);
+    }
+
+    [HttpPut]
+    public void UpdateCustomer(int id, CustomerDto customerDto)
+    {
+      if (!ModelState.IsValid)
+      {
+        throw new HttpResponseException(HttpStatusCode.BadRequest);
+      }
+
+      var dbCustomer = _dbContext.Customers.SingleOrDefault(c => c.Id == id);
+      if (dbCustomer is null)
+      {
+        throw new HttpResponseException(HttpStatusCode.NotFound);
+      }
+
+      Mapper.Map(customerDto, dbCustomer);
+
+      _dbContext.SaveChanges();
+    }
+
+    [HttpDelete]
+    public void DeleteCustomer(int id)
+    {
+      if (!ModelState.IsValid)
+      {
+        throw new HttpResponseException(HttpStatusCode.BadRequest);
+      }
+
+      var dbCustomer = _dbContext.Customers.SingleOrDefault(c => c.Id == id);
+      if (dbCustomer is null)
+      {
+        throw new HttpResponseException(HttpStatusCode.NotFound);
+      }
+
+      _dbContext.Customers.Remove(dbCustomer);
+
+      _dbContext.SaveChanges();
+    }
+  }
+}
